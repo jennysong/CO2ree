0f54fb1f5f657dcf69f047077a6eaf834b1aed59
Jenny <jenny.jieun.song@gmail.com>
2015-03-14 15:48:56 -0700

    fix it!

--- CO2ree/Home.swift
+++ CO2ree/Home.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class Home: UIViewController {
-    var userFirstTime: Bool = true
-    
+    var height:CGFloat?
+    var width:CGFloat?
+    @IBOutlet weak var homeTreeImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    override func viewDidAppear(animated: Bool){
-        super.viewDidAppear(animated)
-        if userFirstTime {
-            self.performSegueWithIdentifier("goToLogIn", sender: self)
-        }
+        navigationController?.navigationBar.barTintColor =  UIColor.whiteColor()
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: Color().get(0x5EBF4F)]
+        height = self.view.frame.size.height
+        width = self.view.frame.size.width
+        let gradient: CAGradientLayer = CAGradientLayer()
+        
+        gradient.colors = [Color().get(0x9AEC5B).CGColor, Color().get(0x5EBF4F).CGColor]
+        gradient.locations = [0.0 , 1.0]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.frame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        self.view.layer.insertSublayer(gradient, atIndex: 0)
+        
+        homeTreeImage.image = UIImage(named: "gittree-white.png")
+        homeTreeImage.frame = CGRectMake(0,0,self.width!*0.6,self.height!*0.5)
+        homeTreeImage.center = self.view.center
+        homeTreeImage.frame.origin = CGPoint(x:self.width!*0.23, y: self.height!*0.15)
     }
     
     override func didReceiveMemoryWarning() {
@@ -29,13 +40,14 @@ class Home: UIViewController {
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
+        /*if segue.identifier == "goToTasks" {
+            let tasksController = segue.destinationViewController as Tasks
+        }*/
+        if segue.identifier == "goToGardenFromHome" {
+            let garden = segue.destinationViewController as Garden
+        }
         if segue.identifier == "goToLogIn" {
             let signUpController = segue.destinationViewController as LogIn
         }
     }
-    
-    @IBAction func goToGarden(sender: AnyObject) {
-        performSegueWithIdentifier("goToGardenFromHome", sender: sender)
-    }
 }
-
