0ca03c9386020b41199570097995b8a5980338f9
Younsuk Oh <younsukoh@Younsuks-Mac.local>
2015-03-14 01:41:09 -0700

    garden

--- CO2ree/Garden.swift
+++ CO2ree/Garden.swift
@@ -1,15 +1,15 @@
 //
-//  ViewController.swift
+//  Garden.swift
 //  CO2ree
 //
-//  Created by Jenny Song on 2015-03-13.
+//  Created by Younsuk Oh on 3/13/15.
 //  Copyright (c) 2015 SOJUniverse. All rights reserved.
 //
 
 import UIKit
 
-class Garden: UIViewController {
-    
+class Garden: UITableViewController {
+    var trees = ["Pine Tree", "Tree", "Git Tree"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +20,18 @@ class Garden: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return trees.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCellWithIdentifier("gardenCell", forIndexPath: indexPath) as MyCustomCell
+        myCell.labelCell.text = trees[indexPath.row]
+        
+        return myCell
+        
+        
+    }
     
 }
 
