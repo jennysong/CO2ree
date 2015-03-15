32e9c0ab38302d57a27c42013a51d80c78da6402
Jenny <jenny.jieun.song@gmail.com>
2015-03-14 18:55:35 -0700

    aaaaaaaaa

---
+++ CO2ree/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  CO2ree
+//
+//  Created by Jenny Song on 2015-03-14.
+//  Copyright (c) 2015 SOJUniverse. All rights reserved.
+//
+
+#ifndef Hello_Header_h
+#define Hello_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+#endif
--- CO2ree/SignUp.swift
+++ CO2ree/SignUp.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SignUp: UIViewController {
     
+    @IBOutlet var loginView: FBLoginView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,12 +28,40 @@ class SignUp: UIViewController {
         if segue.identifier == "goToSignUpWithEmail" {
             let signUpWithEmailController = segue.destinationViewController as SignUpWithEmail
         }
+        else if segue.identifier == "goTosignUpWithFB" {
+            let SignUpWithFBController = segue.destinationViewController as SignUpWithFB
+        }
     }
     
-    
+     /*
     @IBAction func signUpWithEmail(sender: AnyObject) {
             performSegueWithIdentifier("goToSignUpWithEmail", sender: sender)
     }
+    
+   
+    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
+        println("User Logged In")
+    }
+    
+    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
+        println("User: \(user)")
+        var userEmail = user.objectForKey("email") as String
+        
+        var app = UIApplication.sharedApplication().delegate as AppDelegate
+        app.user = User(firstName: user.first_name, lastName: user.last_name, email: userEmail, facebookID: user.objectID)
+        self.performSegueWithIdentifier("goTosignUpWithFB", sender: self)
+        
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
+        println("User Logged Out")
+    }
+    
+    func loginView(loginView : FBLoginView!, handleError:NSError) {
+        println("Error: \(handleError.localizedDescription)")
+    }
+    */
+
     
     
 }
