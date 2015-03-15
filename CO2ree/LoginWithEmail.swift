a8719ec8fb865d74f0c4392057e63a8b1f56d812
Shawn Jung <shawn.jung@me.com>
2015-03-15 02:32:36 -0700

    Login view

--- CO2ree/LoginWithEmail.swift
+++ CO2ree/LoginWithEmail.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LoginWithEmail: UIViewController {
     
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,4 +22,37 @@ class LoginWithEmail: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "goToWelcome" {
+            let welcomeController = segue.destinationViewController as Welcome
+        }
+    }
+    
+    @IBAction func signIn(sender: AnyObject) {
+        RESTClient.post("http://code.shawnjung.ca/session",
+            data: [
+                "email": emailField.text,
+                "password": passwordField.text
+            ],
+            success: { data, response in
+                println(data)
+                let app = UIApplication.sharedApplication().delegate as AppDelegate
+                app.user = User(firstName: data["first_name"] as String, lastName: data["last_name"] as String, email: data["email"] as String, country: data["country_code"] as String, province: data["subregion_code"] as String, password: "")
+                app.user.token = data["session_token"] as? String
+                
+                self.performSegueWithIdentifier("goToWelcome", sender: self)
+
+                
+            },
+            error: { data, response in
+                let alertController = UIAlertController(title: "Login Failed", message:
+                    "Please check your email and password.", preferredStyle: UIAlertControllerStyle.Alert)
+                alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default,handler: nil))
+                self.presentViewController(alertController, animated: true, completion: nil)
+
+            }
+        )
+
+        
+    }
 }
